19df4031be847b7437fea443cc5cce7c63cc8648
Eliot Hemingway <ehemingwayforge@gmail.com>
2015-12-12 22:30:35 -0800

    Update CascadeManager.cs
    Added air cascade effect.

--- Scripts/CascadeManager.cs
+++ Scripts/CascadeManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-// TO DO: Implement Air.
+// TO DO: Implement disaster cascades.
 
 public class CascadeManager {
 
@@ -21,7 +21,7 @@ public class CascadeManager {
 	private int height;
 
 	private int[] elevations = new int[]{1,0,1,0,3,1,2,1};
-	//private int[] telements = new int[]{3,2,2,2,0,1,0,1}; // For future air implementation.
+	private int[] telements = new int[]{3,2,2,2,0,1,0,1};
 
 	private List<Tile> affectedTiles;
 	private Queue<Tile> toDo;
@@ -43,7 +43,9 @@ public class CascadeManager {
 		affectedTiles = new List<Tile>();
 		toDo = new Queue<Tile>();
 
-		affectedTiles.Add(currentTile);
+		// Don't want to hit ourselves with air.
+		if (currentElement != TileType.element.AIR)
+			affectedTiles.Add(currentTile);
 		toDo.Enqueue(currentTile);
 		// Now we can assume in each iteration that we've already added the tile on toDo.
 		// (reasonable, since it'll have been done by the prior iteration.)
@@ -66,11 +68,14 @@ public class CascadeManager {
 			}
 		}
 
+		// If air, here we need to change the current element according to the tile we're hitting with air.
+		if (currentElement == TileType.element.AIR)
+			currentElement = telements[(int)currentTile.type];
+
 		// Then apply the element effect to every tile in affectedTiles.  I don't currently know what to call for that.
-		foreach (Tile t in affectedTiles) 
-		{
+		foreach (Tile t in affectedTiles) {
 			t.Change(currentElement);
-			manager.Change(manager.objectFromTile[t],t);
+			manager.Change(manager.objectFromTile[t].t);
 		}
 	}
 
@@ -197,11 +202,59 @@ public class CascadeManager {
 				affectedTiles.Add(currentCheck);
 			}
 		}
+
 	}
 
 	private void OnAir (Tile currentTile) {
 		// Four checks
+		int coordX = currentTile.x;
+		int coordY = currentTile.y;
 
+		// Four checks
+		Tile currentCheck;
+
+		if (coordY != 0) {
+			currentCheck = manager.getTile[coordX, coordY-1];
+			if (currentCheck.type != 6 && currentCheck.type != 4) {
+				affectedTiles.Add(currentCheck);
+			}
+		}
+
+		if (coordX != 0) {
+			currentCheck = manager.getTile[coordX-1, coordY];
+			if (currentCheck.type != 6 && currentCheck.type != 4) {
+				affectedTiles.Add(currentCheck);
+			}
+		}
+
+		if (coordX != width-1) { 
+			currentCheck = manager.getTile[coordX+1, coordY];
+			if (currentCheck.type != 6 && currentCheck.type != 4) {
+				affectedTiles.Add(currentCheck);
+			}
+		}
+
+		if (coordY != height-1) { 
+			currentCheck = manager.getTile[coordX, coordY+1];
+			if (currentCheck.type != 6 && currentCheck.type != 4) {
+				affectedTiles.Add(currentCheck);
+			}
+		}
 		// Currently air doesn't do anything anyway, so... take a moment.
+	}
+
+	// Spread air, and if it's spread to an existing plains or desert then spread the storm further.
+	public void OnStorm (Tile currentTile) {
+
+	}
+
+	// Spread fire.
+	public void OnEruption (Tile currentTile) {
+
+	}
+
+	// This puppy should cascade, complete with toDo, until it finds an appropriate tile to spread water to.
+	public void OnFlood (Tile currentTile) {
+
 	}
 }
