d1a3d89572f40240f2afed985772000508ef8f49
lizaRyker <dustypallor@gmail.com>
2015-12-12 18:34:07 -0800

    Actually implemented some form of camera control
    Broken at the moment. Looking for center tile to look at (weird errors)
    and deciding camera positioning. Waiting on Jake's code.

--- Scripts/CameraControl.cs
+++ Scripts/CameraControl.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour 
+{
+	public TileManager tiles;
+	float yHeight;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Init()
+	{
+		yHeight = tiles.boardSize.x;
+		int centerX = (int)(tiles.getTile.GetLength (0) / 2);
+		int centerY = (int)(tiles.getTile.GetLength (1) / 2);
+
+		Debug.Log (centerX);
+		Debug.Log (centerY);
+
+		Vector3 LookHere = tiles.tileFromCoordinate [new intVector2 (centerX, centerY)].transform.position;
+
+		transform.LookAt (LookHere);
+
+		transform.position = new Vector3 (tiles.boardSize.x, yHeight, tiles.boardSize.y);
 	}
 }
