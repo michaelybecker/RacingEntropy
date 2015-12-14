685d359dcd04902bdfa96405b0e7936423367dd1
Michael Hazani <123mickey@gmail.com>
2015-12-14 15:31:01 -0800

    night on lose behaviour

---
+++ Scripts/NightLose.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightLose : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("Weather").GetComponent<InstantGoodDay>().StopTime();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Global.lose) {
+			GameObject.Find("Weather").GetComponent<InstantGoodDay>().SetNumericHour(0000);
+//			Debug.Log (GameObject.Find("Weather").GetComponent<InstantGoodDay>().GetMilitaryHour());
+
+		}
+	
+	}
+}
